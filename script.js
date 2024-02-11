0696daec0a2929fe1849044dcf3e41152f473e57
Hazem Hossam <Hazemh.helmy@gmail.com>
2024-02-11 18:05:59 +0200

    add one page

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+
 // Function to toggle overlay visibility
 function toggleOverlay() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = overlay.style.display === "block" ? "none" : "block";
-  }
+  var overlay = document.getElementById("overlay");
+ overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+ }
   
   // Function to activate search
-  function activateSearch() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-  }
+function activateSearch() {
+  var overlay = document.getElementById("overlay");
+ overlay.style.display = "block";
+}
+
+
+
+
